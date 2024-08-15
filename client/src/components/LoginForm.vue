dad8ad3702f9797745df722a8cb233d41c343569
Noctua <titgeogeo@gmail.com>
2024-08-15 18:19:37 +0200

    Update App.vue to use auth store for navigation display

---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,14 @@
+<script setup>
+	import { ref } from 'vue';
+
+	const email = ref('');
+	const password =  ref('');
+	const rememberMe = ref('');
+
+</script>
+
+<template>
+   <div>
+      
+   </div>
+</template>
