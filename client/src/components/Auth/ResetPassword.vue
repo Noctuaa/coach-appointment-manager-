55034d1075b1631a894be56371924feee9c7a43a
Noctua <titgeogeo@gmail.com>
2024-08-16 15:38:56 +0200

    refactor: update imports and references for new Auth structure

---
+++ client/src/components/Auth/ResetPassword.vue
@@ -0,0 +1,26 @@
+<script setup>
+   import { ref } from 'vue';
+
+   const email = ref('');
+   
+</script>
+
+<template>
+		<div class="form forget">
+			<h2>Récupérer mon mot de passe</h2>
+			<form submit.prevent="handleSubmit">
+				<div class="form-group field">
+					<label for="email">Email :</label>
+					<input type="email" id="email" v-model="email" required>
+				</div>
+				<div class="reset-password">
+					<a href="#">Connexion</a>
+				</div>
+				
+				<div class="field">
+					<button>Envoyer</button>
+				</div>
+				
+			</form>
+      </div>
+</template>
---
+++ client/src/views/AuthView.vue
@@ -0,0 +1,17 @@
+<script setup>
+   import { ref } from 'vue';
+   import LoginForm from '@/components/Auth/LoginForm.vue';
+   import ResetPassword from '@/components/Auth/ResetPassword.vue';
+
+   const isActive = ref(false);
+
+
+</script>
+
+<template>
+   <div class="form-container" :class="{activeToggle: isActive}">
+      <div class="img_login toggle-img" ></div>
+      <ResetPassword/>
+      <LoginForm/>
+   </div>
+</template>
