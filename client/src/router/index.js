6bd3101628ce53039116b083b51cc4c8160450fc
Noctua <titgeogeo@gmail.com>
2024-08-25 17:55:22 +0200

    chore: install bcrypt for password hashing

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import AuthView from '@/views/AuthView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'Login',
+      component: AuthView
+    },
+    {
+      path:'/home',
       name: 'home',
       component: HomeView
     },
---
+++ client/src/views/AuthView.vue
@@ -0,0 +1,26 @@
+<script setup>
+   import { ref } from 'vue';
+   import LoginForm from '@/components/Auth/LoginForm.vue';
+   import ResetPassword from '@/components/Auth/ResetPassword.vue';
+
+   const showLogin = ref(true);
+
+   const toggleForm = () => {
+      showLogin.value = !showLogin.value
+   }
+
+</script>
+
+<template>
+   <div class="auth-container" :class="{'toggle-form': !showLogin}">
+      <div class="auth-img"></div>
+      <transition name="fade" mode="out-in">
+         <ResetPassword v-if="!showLogin" @back-to-login="toggleForm"/>
+         <LoginForm v-model:showLogin="showLogin" @forgot-password="toggleForm" v-else />
+      </transition>
+   </div>
+</template>
+
+<style scoped>
+   @import url('../assets/styles/layouts/_auth.css');
+</style>
