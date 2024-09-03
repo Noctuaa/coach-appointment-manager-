1bb96cfe974f5695be1265d69a7442ccc244a03b
Noctua <titgeogeo@gmail.com>
2024-09-03 18:45:31 +0200

    feat: Add role when creating user using objection transaction

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -1,14 +1,14 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+import Role from '../models/Role.js';
+import { transaction } from 'objection';
 
 class AuthController {
 
    static async signup (req, res) {
       try {
-         const { email, password } = req.body;
-
-         console.log(email, password)
+         const { email, password, role = 'user' } = req.body;
 
          // Vérifier si l'utilisateur existe déjà
          const emailIsExist = await User.query().findOne({ email });
@@ -16,14 +16,31 @@ class AuthController {
 
          const hashedPassword = await bcrypt.hash(password, 10);
 
-         const user = await User.query().insert({
-            email: email,
-            password: hashedPassword
-         });
+         // Utilisation d'une transaction pour garantir l'intégrité des données
+         const result = await User.transaction (async trx => {
+            
+            // Récupération de l'ID du rôle
+            const roleRecord = await Role.query(trx).findOne({ name: role });
 
-         const token = jwt.sign({ id : user.id }, process.env.JWT_SECRET, { expiresIn: '1h'});
+            if (!roleRecord) {
+               throw new Error('Rôle non trouvé');
+             }
 
-         res.status(201).json({ user: { id: user.id, email: user.email }, token });
+            //Création de l'utilisateur
+            const user = await User.query(trx).insert({
+               email: email,
+               password: hashedPassword
+            });
+
+            // Ajout de l'entrée dans users_roles
+            await user.$relatedQuery('roles', trx).relate(roleRecord.id);
+
+            return user;
+         })
+
+         const token = jwt.sign({ id : result.id }, process.env.JWT_SECRET, { expiresIn: '1h'});
+
+         res.status(201).json({ user: { id: result.id, email: result.email, role:role }, token });
       } catch (error) {
          res.status(400).json({ message: `Erreur lors de la création de l'utilisateur'`, error: error.message });
       }
