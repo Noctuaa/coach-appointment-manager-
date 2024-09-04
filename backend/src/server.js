9c53838a2325790fa2685b163b0c7fb972e46038
Noctua <titgeogeo@gmail.com>
2024-09-04 18:16:02 +0200

    chore: Add cookie-parser to the project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
 
 import Knex from 'knex';
 import { Model } from 'objection';
@@ -19,6 +20,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 // Route Auth
 app.use('/api/auth', authRoutes);
