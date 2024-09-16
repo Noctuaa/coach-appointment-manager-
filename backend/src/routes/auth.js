6212335e3b8f67556af923a79e31ca6036e889f8
Noctua <titgeogeo@gmail.com>
2024-09-16 16:39:37 +0200

    [Backend] Apply validation for registration and login route

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import AuthController from '../controllers/AuthController.js';
+import { validateLogin, validateSignup } from '../validations/authValidations.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 import roleMiddleware from '../middleware/roleMiddleware.js';
+import AuthController from '../controllers/AuthController.js';
+
+
 
 const router = express.Router();
 
-router.post('/signup', AuthController.signup);
-router.post('/login', AuthController.login);
+router.post('/signup', validateSignup, AuthController.signup);
+router.post('/login', validateLogin , AuthController.login);
 router.post('/refresh', AuthController.refresh);
 router.post('/logout', authMiddleware, roleMiddleware(['user']), AuthController.logout);
 router.get('/me', authMiddleware, roleMiddleware(['user']), AuthController.me);
